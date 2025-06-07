370743a5a0870fa0dc2f3122059d88a498ac100f
RainLumosTaipei <zhou_zhengming@foxmail.com>
2025-06-07 11:16:32 +0800

    using nloman json to serialize

---
+++ include/util/JsonHelper.h
@@ -0,0 +1,10 @@
+//
+// Created by zhou_zhengming on 2025/6/7.
+//
+
+#ifndef CHARGE_SYSTEM_JSONHELPER_H
+#define CHARGE_SYSTEM_JSONHELPER_H
+
+#include "nlohmann/json.h"
+
+#endif //CHARGE_SYSTEM_JSONHELPER_H
