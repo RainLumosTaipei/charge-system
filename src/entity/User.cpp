273d92a369bf7d3463608fb71fdc998ff8942160
RainLumosTaipei <zhou_zhengming@foxmail.com>
2025-06-07 11:16:12 +0800

    add entity po : User, Order

---
+++ src/entity/User.cpp
@@ -0,0 +1,36 @@
+//
+// Created by zhou_zhengming on 2025/6/7.
+//
+
+#include "entity/User.h"
+#include "util/JsonHelper.h"
+
+const std::string &User::getName() const {
+    return name;
+}
+
+void User::setName(const std::string &name) {
+    User::name = name;
+}
+
+const std::string &User::getPassword() const {
+    return password;
+}
+
+void User::setPassword(const std::string &password) {
+    User::password = password;
+}
+
+const std::vector<Order> &User::getOrders() const {
+    return orders;
+}
+
+void User::setOrders(const std::vector<Order> &orders) {
+    User::orders = orders;
+}
+
+void to_json(nlohmann::json& j, const User& u) {
+    j["name"] = u.getName();
+    j["password"] = u.getPassword();
+    j["orders"] = u.getOrders();
+}
